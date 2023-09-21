41331565cf91c20a31b22534d1c396dec641df52
zeeshanmughaltherightsw <106729909+zeeshanmughaltherightsw@users.noreply.github.com>
2023-09-21 22:45:05 +0500

    Push the right code

--- pages/aboutme/index.js
+++ pages/aboutme/index.js
@@ -13,8 +13,9 @@ function AboutMe() {
 
         <Text textAlign={'center'} fontSize={{base:"13px", sm:"16px", md:"18px" }} color='white' fontWeight={600} w='85%' style={{animationDelay: '500ms' }}
             maxW={'900px'} lineHeight={{base:"22px", sm:"28px", md:"32px" }} className="animate__animated animate__fadeIn"
-          />
+          >
           My name is Christina Tyler and I had started Speedy Certz so that way people would have a quicker and more efficient way to get certified so you’re not having to waste any time. I’m constantly having things improved based on the feedback from the students. I started my business after going through negative experiences of people not knowing what to do in emergency situations and having to get my food handlers certification and getting annoyed that every certification that i needed required a different website. I wanted to offer classes that is one place to get every certification that a person would need that allows businesses to be able to use for free. My goal by creating this platform was to create a new pathway out of poverty by providing certifications that you can obtain quickly and efficiently. As someone who has several disabilities and is a disability advocate, I wanted to help change the statistics and be a part of the solution to fixing unemployment.
+        </Text>
     </Flex>
   )
 }
