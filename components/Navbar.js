21a0afb1389083a84865f0ae6d9c53506d62dbb8
zeeshanmughaltherightsw <106729909+zeeshanmughaltherightsw@users.noreply.github.com>
2023-10-01 10:23:46 +0500

    Home page adjusted

--- components/Navbar.js
+++ components/Navbar.js
@@ -15,7 +15,7 @@ function Navbar() {
   
 
   const navLinks = [
-    {text: "Home" , href: "/", delay: 200}, {text: "Courses Explanation" ,href: "/#", delay: 300},{text: "About Me" , href: "/aboutme", delay: 400}
+    {text: "Home" , href: "/", delay: 200}, {text: "About Me" , href: "/aboutme", delay: 400}
   ];
 
  
@@ -110,23 +110,7 @@ function Navbar() {
 
 
 
-      <Flex direction={'row'} align="center" justify={'center'} h='100%'>  
-      
-        <Link href="/" _hover={{textDecoration: 'none'}} >
-          <Button h={{base:"38px", sm:"42px", md:"48px" }} p='12px 28px' fontSize={{base:"13px", sm:"14px", md:"16px" }} 
-            bg={'#38aeb5'} 
-            color={'black'} borderRadius='25px'
-            className="animate__animated animate__fadeInDown"
-            style={{animationDelay: '600ms' }} 
-            fontWeight={600}
-            border='2px solid'
-            borderColor={'#38aeb5'}
-            transition={'all 0.2s linear'}
-            _active={{bgColor:'rgba(196, 140, 68, 0.3)'}} _hover={{bgColor:'rgba(0, 0, 0,0.02)', color:'white', borderColor:'white'}}
-          > 
-            Join Now
-          </Button> 
-        </Link>
+      <Flex direction={'row'} align="center" justify={'center'} h='100%' w='120px'>  
          
       </Flex>
     
--- pagesSections/HomeSection1.js
+++ pagesSections/HomeSection1.js
@@ -11,14 +11,11 @@ import Plx from "react-plx";
 
 
 function HomeSection1() {
-
-
     const contact = [
         {title:'PHONE', value:'(623)330-9456', icon: FaPhoneAlt},
         {title:'EMAIL', value:'SpeedyCertz@gmail.com', icon: FaEnvelope},
         {title:'OWNER', value:'Christina Tyler', icon: FaUser}
     ];
-
     const parallaxData = [
         {
             start: 0,
@@ -89,47 +86,6 @@ function HomeSection1() {
       >
 
 
-        <Image src='diploma.png' maxH={{base:"35px", sm:"45px", md:"55px" }} />
- 
-        <Fade delay={200} bottom> 
-          <Text textAlign={'center'} fontSize={{base:"22px", sm:"30px", md:"40px" }} color='white' fontWeight={700} 
-            lineHeight={{base:"28px", sm:"38px", md:"48px" }} w='100%' letterSpacing={'1px'} maxW={'700px'}
-          >
-            <span style={{color:'#45ced6'}}>Speedy</span> Certz, is The <br/>Speedy <span style={{color:'#45ced6'}}>Way</span>
-          </Text>
-        </Fade>
-
-        <Fade delay={300} bottom> 
-          <Text textAlign={'center'} fontSize={{base:"14px", sm:"16px", md:"18px" }} color='white' fontWeight={500} w='100%' 
-            maxW={'700px'} lineHeight={{base:"18px", sm:"22px", md:"26px" }}
-          >
-            {/*Empower yourself with American Red Cross: First Aid, CPR, AED, and Basic Life Support. */}
-            Explore our versatile range of blended courses, combining hands-on experience with comprehensive certifications. 
-            Your journey to expertise and professional recognition starts today.
-          </Text>
-        </Fade>
-
-        <Fade delay={400} bottom> 
-          <Flex direction={'row'} bgColor={'rgba(49, 89, 89,0.2)'} p='10px' gap='5px' borderRadius={'15px'} justify={'center'} align={'center'} w='fit-content'>
-            <Icon as={FcOnlineSupport} fontSize={{base:"20px", sm:"22px", md:"24px" }} />
-            <Text fontSize={{base:"12px", sm:"14px", md:"16px" }} color='#3ab6bd' fontWeight={600} letterSpacing={'0.5px'} textAlign={'center'}>
-              To make sure you sign up for the correct course,<br/> Check the course explanation page to understand the different courses.
-            </Text>
-            <Icon as={FcOnlineSupport} fontSize={{base:"20px", sm:"22px", md:"24px" }} />
-          </Flex> 
-        </Fade>
-
-        <Fade delay={500} bottom> 
-          <Flex direction={'row'} gap='5px' justify={'center'} align={'center'} w='fit-content' color='#FFD700'>
-            <Icon as={TbDiscount2} fontSize={{base:"20px", sm:"22px", md:"24px" }} />
-            <Text w='100%' maxW={'700px'} textAlign={'center'} fontWeight={700} fontSize={{base:"12px", sm:"14px", md:"16px" }} mx={'auto'}  >
-              Contact us For Business Wholesale Discounts
-            </Text>
-            <Icon as={TbDiscount2} fontSize={{base:"20px", sm:"22px", md:"24px" }} />
-          </Flex>
-        </Fade>
-
-
 
         {/*<Flex direction={{base:"column", sm:"column", md:"row" }} w={{base:"100%", sm:"100%", md:"fit-content" }}
           align={'start'} gap={{base:"10px", sm:"15px", md:"25px" }} justify={'start'} wrap={'wrap'}
@@ -164,8 +120,6 @@ function HomeSection1() {
           </Fade>
           
           </Flex>*/}
-
-
         <Image src='license.png' alt='license' maxW={'500px'} w='100%' />
 
       </Flex>
@@ -186,18 +140,6 @@ function HomeSection1() {
             />
           </Plx>
         </Box>
-    
-        <Box w='fit-content' bottom={"10%"} zIndex={1} right={'8%' } position={'absolute'} textAlign='center' >
-          <Plx className="MyAwesomeParallax" parallaxData={parallaxData3}>
-            <Image src='g4.png' maxW={{base:"4vw", sm:"5vw", md:"6vw" }} minW={'80px'} opacity={'0.2'} />
-          </Plx>
-        </Box>
-
-        <Box w='fit-content' top={"10%" } zIndex={1} left='7%' position={'absolute'} textAlign='center' >
-          <Plx className="MyAwesomeParallax" parallaxData={parallaxData}>
-            <Image src='g3.png' maxW={{base:"4vw", sm:"5vw", md:"6vw" }} minW={'80px'} opacity={'0.2'} />
-          </Plx>
-        </Box> 
  
 
     </Flex>
